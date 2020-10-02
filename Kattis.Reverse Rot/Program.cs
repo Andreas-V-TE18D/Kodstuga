ecc446de3e0e0e30e6ea1a1a761e5f6c53d97e63
Andreas-V-TE18D <54800767+Andreas-V-TE18D@users.noreply.github.com>
2020-10-02 12:19:26 +0200

    planerat hur programet ska se ut

--- Kattis.Reverse Rot/Program.cs
+++ Kattis.Reverse Rot/Program.cs
@@ -7,7 +7,10 @@ namespace Kattis.Reverse_Rot
         static void Main(string[] args)
         {
             string hemlig = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_.";
-
+            // Först ska man ta in medelandet och antalet gånger den ska roteras 
+            // sen ska medellandet omvändas abcd till dcba 
+            // efter det ska medelandet framföras x antal gånger i alfabetet 
+            // till exempel blir dcba till edcb med (1) skuts framåt. 
           Console.WriteLine("Kattis Reverse Rot");
 
             
